f2417760ebd974e46d2732cb5d19d50837bc6eec
Kevinsal03 <kevinsal03@users.noreply.github.com>
2021-01-08 13:33:44 -0500

    Add Metrics and H2 dep. Add shading

--- src/main/java/me/kevsal/minecraft/skyblockminions/SkyblockMinions.java
+++ src/main/java/me/kevsal/minecraft/skyblockminions/SkyblockMinions.java
@@ -1,15 +1,22 @@
 package me.kevsal.minecraft.skyblockminions;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkyblockMinions extends JavaPlugin {
 
     private static SkyblockMinions instance;
 
+    // Metrics plugin ID
+    private static final int BSTATS_METRICS_PLUGIN_ID = 9939;
+
     @Override
     public void onEnable() {
         // Set instance of plugin when loaded by server
         instance = this;
+
+        // Enable metrics
+        Metrics metrics = new Metrics(this, BSTATS_METRICS_PLUGIN_ID);
     }
 
     @Override
